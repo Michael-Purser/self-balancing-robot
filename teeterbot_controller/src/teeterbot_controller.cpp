791c916be8dfc6786f836d344f6d7b4f64e93d6b
Michael Purser <michael.purser@outlook.be>
2021-03-09 22:32:33 +0000

    [teeterbot_controller] Add proportional square root controller

--- teeterbot_controller/src/teeterbot_controller.cpp
+++ teeterbot_controller/src/teeterbot_controller.cpp
@@ -1,17 +1,35 @@
 #include <ros/console.h>
+#include <ros/duration.h>
 #include <ros/init.h>
 #include <ros/node_handle.h>
 #include <ros/rate.h>
 #include <ros/subscriber.h>
+#include <ros/publisher.h>
 
 #include <sensor_msgs/Imu.h>
+#include <std_msgs/Float64.h>
+#include <tf/tf.h>
 
+#include <cmath>
 #include <iostream>
+#include <stdlib.h>
+
+double robot_pitch_setpoint_ = 0.0;
+double robot_pitch_;
+double pid_proportional_gain_ = 1.0;
 
 void
-imuSubscriberCallback(const sensor_msgs::Imu& msg)
+imuSubscriberCallback(const sensor_msgs::ImuConstPtr msg)
 {
-  ROS_INFO_STREAM("Orientation x: " << msg.orientation.x);
+  tf::Quaternion quaternion(
+      msg->orientation.x,
+      msg->orientation.y,
+      msg->orientation.z,
+      msg->orientation.w);
+  tf::Matrix3x3 matrix(quaternion);
+  double roll, yaw;
+  matrix.getRPY(roll, robot_pitch_, yaw);
+  // ROS_INFO_STREAM("Orientation pitch: " << robot_pitch_);
 }
 
 int
@@ -22,11 +40,37 @@ main(int argc, char** argv)
 
   ros::Subscriber imu_subscriber {nh.subscribe("/teeterbot/imu", 1, imuSubscriberCallback)};
 
+  ros::Publisher left_wheel_publisher {nh.advertise<std_msgs::Float64>("/teeterbot/left_speed_cmd", 1)};
+  ros::Publisher right_wheel_publisher {nh.advertise<std_msgs::Float64>("/teeterbot/right_speed_cmd", 1)};
+
+  nh.getParam("/teeterbot_controller/proportional_gain", pid_proportional_gain_);
+  ROS_INFO_STREAM("Controller Proportional Gain: " << pid_proportional_gain_);
+
+  ros::Duration(2.0).sleep();
+
   ros::Rate rate(100.0);
+
+  std_msgs::Float64 msg;
 
   while (ros::ok())
   {
     ros::spinOnce();
+
+    double error = robot_pitch_ - robot_pitch_setpoint_;
+
+    if (abs(robot_pitch_) < 0.3)
+    {
+      double sign = signbit(error) ? -1.0 : 1.0;
+      msg.data = sign * pid_proportional_gain_ * std::pow(abs(error), 0.5);
+      ROS_INFO_STREAM("Published control signal: " << msg.data);
+    }
+    else
+    {
+      msg.data = 0.0;
+    }
+    left_wheel_publisher.publish(msg);
+    right_wheel_publisher.publish(msg);
+
     rate.sleep();
   }
 
