aa9835ba8ef96e7a7871e207397a55142dcd436d
Michael Purser <michael.purser@outlook.be>
2021-03-10 11:08:56 +0000

    [pid_controller] Add proportional control scheme
     - Parameters defined in launch file for easy editing
     - Also implement a cutoff angle; if robot tips over that cutoff angle,
    just falls down. Prevents sending too large signals to the motors and
    aloows a better workflow with gazebo (prevents the robot flying off in
    the distance).

--- pid_controller/include/pid_controller/pid_controller.h
+++ pid_controller/include/pid_controller/pid_controller.h
@@ -2,7 +2,9 @@
 #define PID_CONTROLLER_PID_CONTROLLER
 
 #include <ros/node_handle.h>
+#include <ros/rate.h>
 #include <ros/subscriber.h>
+#include <ros/publisher.h>
 
 #include <sensor_msgs/Imu.h>
 
@@ -14,14 +16,21 @@ class PIDController
 public:
   PIDController(ros::NodeHandle& node_handle);
 
-  void spin() const;
+  void spin();
 
 private:
   void imuCallback(const sensor_msgs::ImuConstPtr& imu_msg);
+  void sendControlSignal();
 
   double current_pitch_;
+  double cutoff_pitch_;
+  double pitch_setpoint_;
+  double proportional_control_gain_;
+  double spin_rate_;
 
   ros::Subscriber imu_subscriber_;
+  ros::Publisher left_wheel_publisher_;
+  ros::Publisher right_wheel_publisher_;
 };
 
 }
--- pid_controller/src/pid_controller/pid_controller.cpp
+++ pid_controller/src/pid_controller/pid_controller.cpp
@@ -2,19 +2,35 @@
 #include "pid_controller/pid_controller.h"
 
 #include <ros/console.h>
-#include <ros/rate.h>
+#include <ros/duration.h>
+
+#include <std_msgs/Float64.h>
+
+#include <iostream>
+#include <string>
 
 pid_controller::PIDController::PIDController(ros::NodeHandle& node_handle) :
     current_pitch_ {0.0}
 {
+  node_handle.getParam("/pid_controller/cutoff_pitch", cutoff_pitch_);
+  node_handle.getParam("/pid_controller/pitch_setpoint", pitch_setpoint_);
+  node_handle.getParam("/pid_controller/proportional_control_gain", proportional_control_gain_);
+  node_handle.getParam("/pid_controller/spin_rate", spin_rate_);
+
+  ros::Duration(1.0).sleep();
+
+  std::string left_wheel_topic {"/teeterbot/left_speed_cmd"};
+  std::string right_wheel_topic {"/teeterbot/right_speed_cmd"};
 
   imu_subscriber_ = node_handle.subscribe("/teeterbot/imu", 1, &PIDController::imuCallback, this);
+  left_wheel_publisher_ = node_handle.advertise<std_msgs::Float64>("/teeterbot/left_speed_cmd", 1);
+  right_wheel_publisher_ = node_handle.advertise<std_msgs::Float64>("/teeterbot/right_speed_cmd", 1);
 }
 
 void
-pid_controller::PIDController::spin() const
+pid_controller::PIDController::spin()
 {
-  ros::Rate spin_rate(500.0);
+  ros::Rate spin_rate(spin_rate_);
 
   while(ros::ok())
   {
@@ -28,4 +44,20 @@ void
 pid_controller::PIDController::imuCallback(const sensor_msgs::ImuConstPtr& imu_msg)
 {
   current_pitch_ = getPitchFromImuMessage(imu_msg);
+}
+
+void
+pid_controller::PIDController::sendControlSignal()
+{
+  std_msgs::Float64 msg;
+  if (-cutoff_pitch_ <= current_pitch_ && current_pitch_ <= cutoff_pitch_)
+  {
+    msg.data = proportional_control_gain_ * (current_pitch_ - pitch_setpoint_);
+  }
+  else
+  {
+    msg.data = 0.0;
+  }
+  left_wheel_publisher_.publish(msg);
+  right_wheel_publisher_.publish(msg);
 }
